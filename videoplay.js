14a04bf019ae1c3b2763835916696a72b6c76f50
jm7meme <77819603+jm7meme@users.noreply.github.com>
2022-03-15 20:06:46 +0000

    Update videoplay.js

--- videoplay.js
+++ videoplay.js
@@ -1,10 +1,18 @@
 var player = document.getElementById('videoplay');
 var video = document.getElementById('videoplayer');
-var progressBar = document.getElementById('progress-bar');
+
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    player.addEventListener('ended', ended);
+    var progressBar = document.getElementById('progress-barphone');
+    document.getElementById('phoneplay').textContent = "\u25B7";
+    progressBar.addEventListener('input', changetime);
     player.addEventListener('timeupdate', timeplayed);
-} else {    
+    player.addEventListener('ended', ended);
+    window.addEventListener('keypress', shortcut);
+    var duration = document.getElementById('duration1');
+    var fullscreenicon= document.getElementById('fullscreen1');
+    var timeplayed = document.getElementById('timeplayed1');
+} else{
+    var progressBar = document.getElementById('progress-bar');
     progressBar.addEventListener('input', changetime);
     player.addEventListener('timeupdate', timeplayed);
     video.addEventListener('mouseenter', controlappear);
@@ -13,7 +21,12 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     player.addEventListener('click', animatePlayback);
     player.addEventListener('ended', ended);
     window.addEventListener('keypress', shortcut);
+    var duration = document.getElementById('duration');
+    var timeplayed = document.getElementById('timeplayed');
+    var fullscreenicon= document.getElementById('fullscreen');
 }
+
+
 function shortcut(event) {
     var event = window.event;
     event.preventDefault();
@@ -23,15 +36,15 @@ function shortcut(event) {
                 playvideo();
             } else {
                 initializeVideo();
-            }
-            
+            };
         case 'ArrowLeft':
             player.currentTime = player.currenttime - 5.0 ;
         case 'ArrowRight':
             player.currentTime = player.currenttime + 5.0 ;
         case 'f':
             fullscreen();
-
+        case 'Escape':
+            normalscreen();
     }
 }
 
@@ -45,16 +58,15 @@ function formatTime(timeInSeconds) {
 
 function initializeVideo() {
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        var duration = document.getElementById('duration');
+        document.getElementById('phoneplay').textContent = "\u25B7";
         const videoDuration = Math.round(player.duration);
         const time = formatTime(videoDuration);
         duration.innerText = `${time.minutes}:${time.seconds}`;
         duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`);
         progressBar.setAttribute('max', player.duration);
-        player.setAttribute('onclick', 'playvideo()');
+        player.setAttribute('onclick', 'controlappearphone()');
         playvideo();
     } else{
-        var duration = document.getElementById('duration');
         document.getElementById('playback-animation').style.opacity='0'
         document.getElementById('progress-bar').style.display='block';
         document.getElementById('icon').classList.remove('animationimg')
@@ -75,18 +87,23 @@ function initializeVideo() {
 function playvideo() {
     var element = document.getElementById('play');
     if(player.paused) {
+        document.getElementById('phoneplay').innerHTML='<div class="pausebtn"><i class="playicon"></i> <i class="playicon"></i></div>';
         player.play();
         element.textContent = "\u23F8";
         element.setAttribute('title', 'Pause');
         document.getElementById('icon').src='playicon.png';
-        document.getElementById('icon').style='transform:scale(0.1)';
+        document.getElementById('icon').classList.add('playiconanimate');
+        document.getElementById('icon').classList.remove('pauseiconanimate');
         animatePlayback();
+
     }else {
         player.pause();
+        document.getElementById('phoneplay').textContent = "\u25B7";
         element.textContent = "\u23F5";
         element.setAttribute('title', 'Play');
         document.getElementById('icon').src='pauseicon.png';
-        document.getElementById('icon').style='transform:scale(0.2)';
+        document.getElementById('icon').classList.add('pauseiconanimate');
+        document.getElementById('icon').classList.remove('playiconanimate');
         animatePlayback();
     
     }
@@ -124,7 +141,6 @@ function mute(){
 }
 
 function timeplayed() {
-    var timeplayed = document.getElementById('timeplayed');
     const duration = Math.round(player.currentTime);
     const time = formatTime(duration);
     progressBar.value = player.currentTime;
@@ -178,7 +194,7 @@ function updateFullscreen(){
     var video = document.getElementById('videoplayer');
     document.getElementById('videocontrol').style.width='100%';
     document.getElementById('progress').style.width='99%';
-    document.getElementById('fullscreen').style.marginLeft='57%';
+    fullscreenicon.style.marginLeft='57%';
     document.getElementById('playback-animation').style.marginTop='350px'
     document.getElementById('fullscreen').innerHTML='<span class="iconify" data-icon="mdi:fullscreen-exit" style="color: white;" data-width="31" data-height="31"></span>';
     document.getElementById('fullscreen').setAttribute('title', 'Exit full screen')
@@ -187,7 +203,7 @@ function updateFullscreen(){
 function normalscreen() {
     document.getElementById('videocontrol').style.width='1000px';
     document.getElementById('progress').style.width='99%';
-    document.getElementById('fullscreen').style.marginLeft= '38%';
+    fullscreenicon.style.marginLeft= '38%';
     document.getElementById('playback-animation').style.marginTop='220px'
     document.getElementById('fullscreen').innerHTML='<i class="material-icons" id="fullicon">&#xe5d0;</i>';
 }
@@ -205,6 +221,20 @@ function controldisappear(){
         document.getElementById('videocontrol').style.marginTop='10px';
         document.getElementById('progress').style.marginTop='10px';}, 3000);
     
+}
+var bar = document.getElementById('videocontrolphone');
+var playbutton = document.getElementById('phoneplay');
+function controlappearphone() {
+    bar.style.top='85%';
+    player.style.opacity='0.4';
+    playbutton.style.opacity='1';
+    player.setAttribute('onclick', 'controldisappearphone()');
+}
+function controldisappearphone() {
+    bar.style.top='100%';
+    player.style.opacity='1';
+    playbutton.style.opacity='0';
+    player.setAttribute('onclick', 'controlappearphone()');
 }
 var playbackAnimation = document.getElementById('playback-animation');
 function animatePlayback() {
@@ -258,4 +288,13 @@ function changetime() {
 function ended() {
     var element = document.getElementById('play');
     element.innerHTML='<span class="iconify , replay" data-icon="mdi:replay" data-flip="horizontal"></span>'
+}
+document.getElementById('progress').addEventListener('mouseenter', progressthumb);
+function progressthumb() {
+    progressBar.classList.add('progressthumb');
+    progressBar.classList.remove('progresshide');
+    document.getElementById('progress').addEventListener('mouseleave', function(){
+        progressBar.classList.add('progresshide');
+        progressBar.classList.remove('progressthumb');
+    });
 }
