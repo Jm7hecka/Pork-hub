414c1590e0a4d1b9f7fc84d705aec351708559d0
jm7meme <77819603+jm7meme@users.noreply.github.com>
2022-03-16 01:28:54 +0000

    Update array.php

--- array.php
+++ array.php
@@ -1,21 +1,21 @@
 <?php
-session_start();
-$data = ' ';
-$id=' ';
-$id = $_GET['video'];
-$empty = array('file'=>'',
-              'poster'=>'', 
-              'vidname'=>'', 
-              'view'=> '', 
-              'like'=>'' , 
-              'dislike'=>'' , 
-              'save'=>'' , 
-              'authorname'=>'', 
-              'authorimg'=>'',
-              'originalurl'=>'');
+session_start();    
+if(isset($_GET['video'])){
+    $id = $_GET['video'];   
+}
+$empty = array('file'=>'./video/',
+              'poster'=>'./video/', 
+              'vidname'=>' ', 
+              'view'=> ' ', 
+              'like'=>' ' , 
+              'dislike'=>' ' , 
+              'save'=>' ' , 
+              'authorname'=>' ', 
+              'authorimg'=>' ',
+              'originalurl'=>' ');
 
-$one = array('file'=>'1.mp4',
-             'poster'=>'1.mp4#t=10',
+$one = array('file'=>'./video/1.mp4',
+             'poster'=>'./video/1.mp4#t=10',
              'vidname'=>'Peppa Pig is playing with her family!', 
              'view'=>69, 
              'like'=>48, 
@@ -25,8 +25,8 @@ $one = array('file'=>'1.mp4',
              'authorimg'=>'https://yt3.ggpht.com/ytc/AKedOLREN1pLcplbkzE2HU3h8lZAx_iAwoMhHjLq_p4H=s88-c-k-c0x00ffffff-no-rj',
              'originalurl'=>'https://www.youtube.com/watch?v=mA1UxREUs5c');
 
-$two = array('file'=>'2.mp4', 
-             'poster'=>'2.mp4#t=5', 
+$two = array('file'=>'./video/2.mp4', 
+             'poster'=>'./video/2.mp4#t=5', 
              'vidname'=>'Long pork makes her satisfying!', 
              'view'=>84, 
              'like'=>50, 
@@ -36,8 +36,8 @@ $two = array('file'=>'2.mp4',
              'authorimg'=>'https://yt3.ggpht.com/ytc/AKedOLTqrAiM8eD6IL7MpZGwcx9RHQ9YrNj0tzwWW1Yc=s88-c-k-c0x00ffffff-no-rj',
              'originalurl'=>'https://www.youtube.com/watch?v=L7lHdaKJC8A');
 
-$three = array('file'=>'3.mp4', 
-               'poster'=>'3.mp4#t=5', 
+$three = array('file'=>'./video/3.mp4', 
+               'poster'=>'./video/3.mp4#t=5', 
                'vidname'=>'Hot pan wants to fill up by oil', 
                'view'=>1503, 
                'like'=>400, 
@@ -47,8 +47,8 @@ $three = array('file'=>'3.mp4',
                'authorimg'=>'https://yt3.ggpht.com/YKlHmdQqN2E15D5l6ZDbtGVSkhVMatPiFHd80RNl33ON_ueoJqOdQ4mCarClsrNyf8oEHZZ4XA=s88-c-k-c0x00ffffff-no-rj',
                'originalurl'=>'https://www.youtube.com/watch?v=C6XQuegX-m0');
 
-$four = array('file'=>'4.mp4',
-              'poster'=>'4.mp4#t=2.5', 
+$four = array('file'=>'./video/4.mp4',
+              'poster'=>'./video/4.mp4#t=2.5', 
               'vidname'=>'Our pork hub intro!', 
               'view'=>10000, 
               'like'=>4605, 
@@ -57,6 +57,7 @@ $four = array('file'=>'4.mp4',
               'authorname'=>'Jm7 official', 
               'authorimg'=>'jm7newlogo.png',
               'originalurl'=>'http://localhost:8080/porkhub/viewvideo.php?video=04');
+
 $five = array('file'=>'./video/5.mp4',
               'poster'=>'./video/5.mp4#t=24', 
               'vidname'=>'Fat pork is attracted by hot pot!', 
@@ -68,5 +69,8 @@ $five = array('file'=>'./video/5.mp4',
               'authorimg'=>'https://yt3.ggpht.com/48uvMpwYcLKZOI_PRRNkI6GTEwkZHzWPpGkp3R-OpcW_g_qfqB6jOi_ZRc3hj6SFIaDDGs4zyR0=s88-c-k-c0x00ffffff-no-rj',
               'originalurl'=>'https://www.youtube.com/watch?v=UpKJuGQ9c0U');
 $idarray = array('01'=> $one, '02'=> $two, '03'=> $three, '04'=> $four, '05'=>$five);
-$_SESSION['array'] = $idarray[$id];
+if(isset($id)){
+    $_SESSION['array'] = $idarray[$id];
+}
+$_SESSION['idarray'] = $idarray;
 ?>
