8eac639aa509defe0ef2fbf77ea4033a59c2f2a2
jm7meme <77819603+jm7meme@users.noreply.github.com>
2022-03-19 21:18:00 +0000

    Update viewvideo.php

--- viewvideo.php
+++ viewvideo.php
@@ -13,7 +13,7 @@
     <?php 
      echo '<title>'.$data["vidname"].'</title>'
     ?>
-    <link rel="icon" href="porkhublogo.ico" />
+    <link rel="icon" href="porkhubicon.png" />
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <link rel="stylesheet" href="styles.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
